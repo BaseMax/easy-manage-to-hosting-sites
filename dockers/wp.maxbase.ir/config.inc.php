ad59f95e57738f55ae530bb8c05700efc55d8588
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-02 08:54:59 +0100

    up

---
+++ dockers/wp.maxbase.ir/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+$cfg['blowfish_secret'] = 'abmkDJIFGJI345JIOjiofgm>,.4345ji';
+
+$cfg['Servers'][1]['host'] = 'mariadb';
+$cfg['Servers'][1]['port'] = '3306';
+
+$cfg['Servers'][1]['auth_type'] = 'cookie';
