0e6d6b6c54d03eef948b35757711dac1efee755c
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-06 22:36:06 +0100

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Stop and remove all running containers
+docker-compose down
+
+# Stop and remove all containers
+docker stop $(docker ps -q)
+docker rm $(docker ps -aq)
+
+# Remove all Docker images
+docker rmi $(docker images -q)
+
+# Prune unused volumes and networks
+docker volume prune -f
+docker network prune -f
