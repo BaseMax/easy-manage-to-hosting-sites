a7c8e1ca957b0d45900e49dcc4ea0c30aad16b50
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-06 21:15:45 +0100

    delete logs/filebrowser, add prefix to .env, update dockerfile of filemanager

--- template/config/phpmyadmin.php
+++ template/config/phpmyadmin.php
@@ -6,8 +6,8 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mariadb';
 $cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = getenv('MARIADB_ROOT_PASSWORD');
+// $cfg['Servers'][$i]['user'] = 'root';
+// $cfg['Servers'][$i]['password'] = getenv('MARIADB_ROOT_PASSWORD');
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
